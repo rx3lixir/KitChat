c1353c7ba40d11b0ead346beb49d457b53eaf1b5
I027356 <autotune1@icloud.com>
2023-09-18 11:32:43 +0700

    initial setup, dark mode and other stuff

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
 import { UserButton } from "@clerk/nextjs/app-beta";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl='/' />
+      <ModeToggle />
     </div>
   );
 }
