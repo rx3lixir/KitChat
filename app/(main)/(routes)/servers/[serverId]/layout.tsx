da97988e8f1120c59a3fe1db7fc78021e7e690b3
I027356 <autotune1@icloud.com>
2023-09-20 20:44:20 +0700

    server sidebar and dropdowns created

---
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -0,0 +1,40 @@
+import { currentProfile } from "@/lib/current-profile";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { ServerSidebar } from "@/components/server/server-sidebar";
+
+const ServerIdLayout = async ({
+  params,
+  children,
+}: {
+  params: { serverId: string };
+  children: React.ReactNode;
+}) => {
+  const profile = await currentProfile();
+
+  if (!profile) return redirectToSignIn();
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: { profileId: profile.id },
+      },
+    },
+  });
+
+  if (!server) return redirect("/");
+
+  return (
+    <div className='h-full'>
+      <div className='hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0'>
+        <ServerSidebar serverId={params.serverId} />
+      </div>
+
+      <main className='h-full md:pl-60'>{children}</main>
+    </div>
+  );
+};
+
+export default ServerIdLayout;
