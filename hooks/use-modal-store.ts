7d219eaf24fe90d63f05f2f4447b35e2c4978aad
I027356 <autotune1@icloud.com>
2023-09-20 16:44:42 +0700

    finished sidebar working

---
+++ hooks/use-modal-store.ts
@@ -0,0 +1,19 @@
+import { type } from "os";
+import { set } from "zod";
+import { create } from "zustand";
+
+export type ModalType = "createServer";
+
+interface ModalStore {
+  type: ModalType | null;
+  isOpen: boolean;
+  onOpen: (type: ModalType) => void;
+  onClose: () => void;
+}
+
+export const useModal = create<ModalStore>((set) => ({
+  type: null,
+  isOpen: false,
+  onOpen: (type) => set({ isOpen: true, type }),
+  onClose: () => set({ type: null, isOpen: false }),
+}));
