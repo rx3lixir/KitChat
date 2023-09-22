38d3f350348641e0a220b58c1da48acb215ce60a
ISIXONEISIXONE <i027356@inbox.ru>
2023-09-22 12:29:36 +0700

    server settings routes providing

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,7 @@
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { useEffect, useState } from "react";
 import { InviteModal } from "@/components/modals/invite-modal";
+import { EditServerModal } from "@/components/modals/edit-server";
 
 export const ModalProvider = () => {
   // Prevets modals to be rendered on a server side
@@ -18,6 +19,7 @@ export const ModalProvider = () => {
     <>
       <CreateServerModal />
       <InviteModal />
+      <EditServerModal />
     </>
   );
 };
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -51,7 +51,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className='px-3 py-2 text-sm cursor-pointer'>
+          <DropdownMenuItem
+            onClick={() => onOpen("editServer", { server })}
+            className='px-3 py-2 text-sm cursor-pointer'
+          >
             Server Settings
             <Settings className='h-4 w-4 ml-auto' />
           </DropdownMenuItem>
