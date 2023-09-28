cd8ba4e50ff453839c71c0a2be0046cb6dbfe14e
ISIXONEISIXONE <i027356@inbox.ru>
2023-09-28 16:07:28 +0700

    channel actions provide

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -8,6 +8,9 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { ServerHeader } from "./server-header";
 import { ServerSearch } from "./server-search";
 import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { ServerSection } from "./server-section";
+import { ServerChannel } from "./server-channel";
 
 interface ServerSidebarProps {
   serverId: string;
@@ -116,6 +119,43 @@ export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
             ]}
           />
         </div>
+        <Separator className='bg-zinc-200 dark:bg-zinc-700 rounded-md my-2' />
+        {!!textChannels?.length && (
+          <div className='mb-2'>
+            <ServerSection
+              sectionType='channels'
+              channelType={ChannelType.TEXT}
+              role={role}
+              label='Text Channels'
+            />
+            {textChannels.map((channel) => (
+              <ServerChannel
+                key={channel.id}
+                channel={channel}
+                role={role}
+                server={server}
+              />
+            ))}
+          </div>
+        )}
+        {!!audioChannels?.length && (
+          <div className='mb-2'>
+            <ServerSection
+              sectionType='channels'
+              channelType={ChannelType.AUDIO}
+              role={role}
+              label='Voice Channels'
+            />
+            {audioChannels.map((channel) => (
+              <ServerChannel
+                key={channel.id}
+                channel={channel}
+                role={role}
+                server={server}
+              />
+            ))}
+          </div>
+        )}
       </ScrollArea>
     </div>
   );
